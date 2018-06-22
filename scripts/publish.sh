cbf83cd2ab1ab001b5322b1ebe8f34bf69bd9ce7
ilokhat <lartane@gmail.com>
2018-06-22 18:18:41 +0200

    excluding node_modules copy between master and gh-pages

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,8 +8,13 @@ echo " # Publish the gh-pages"
 cd master && COMMIT=`git rev-parse HEAD`
 # remove all current files but .gitignore
 cd ../gh-pages && git rm -qrf . && git checkout HEAD -- .gitignore
-# copy non-dev files from master to gh-pages directory
-cp -r ../master/* .
+# copy non-dev files from master to gh-pages directory, excluding node-modules for windows compatibility
+#cp -r ../master/* .
+cd ../master
+cp -r `ls ../master | grep -v "node_modules"` ../gh-pages
+cd ../gh-pages
+# we remove first, windows compatibility
+rm -rf doc
 mkdir doc && cp -r ../itowns/out doc/itowns
 # commit and push all files
 git add --all && git commit -m "Publish from master ($COMMIT)" && git push origin gh-pages
